a662fef4fdbb830946162e7588c5da8198ac9ba0
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-06-09 13:20:06 +0200

    Add create new post.

--- api-routes/posts.js
+++ api-routes/posts.js
@@ -1,9 +1,34 @@
+import { supabase } from "@/lib/supabaseClient";
+
 export const getPosts = () => {
   //Handle get all posts
 };
 
-export const addPost = () => {
-  //Handle add post here
+export const addPost = async ({ editorContent, titleInput, image, newPostSlug }) => {
+  try {
+	  const { data, error, status } = await supabase
+		.from("blog-data")
+		.insert(
+      {
+        title: titleInput,
+        slug: newPostSlug,
+        // created_at: timestamptz,
+        body: editorContent
+      }
+    )
+		// .eq("slug", slug)
+		// .single();
+  
+		console.log("addPost", titleInput, newPostSlug, editorContent);
+
+	  if (error) {
+		return { error: 'Something has gone wrong.' };
+	  }
+  
+	  return { data, status, error };
+	} catch (error) {
+	  console.log(error);
+	}
 };
 
 export const removePost = () => {
--- pages/create-post/index.jsx
+++ pages/create-post/index.jsx
@@ -1,10 +1,19 @@
 import BlogEditor from "@/components/blog-editor";
 import { createSlug } from "@/utils/createSlug";
+import { addPost } from "../../api-routes/posts";
+import { useRouter } from "next/router";
+
 
 export default function CreatePost() {
+  const router = useRouter();
   const handleOnSubmit = ({ editorContent, titleInput, image }) => {
-    const slug = createSlug(titleInput);
-    console.log({ editorContent, titleInput, image, slug });
+    const newPostSlug = createSlug(titleInput).toLowerCase();
+    addPost({ editorContent, titleInput, image, newPostSlug });
+    
+    console.log("Add Post", { editorContent, titleInput, image, newPostSlug });
+    
+    return router.push(`/blog/${newPostSlug}`);
+    
   };
 
   return (
