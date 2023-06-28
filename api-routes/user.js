c855bde569661e66c7c20219213c869c3132dbcf
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-06-28 12:24:14 +0200

    Add Account page.

--- api-routes/user.js
+++ api-routes/user.js
@@ -1,31 +1,11 @@
 import { supabase } from "@/lib/supabaseClient";
 
-export const getUser = async (_, { arg: id }) => {
-  console.log("id i users " + id);
-
+export const getUser = async (id) => {
     const { data, error, status } = await supabase
       .from("users")
       .select("*")
       .single()
       .eq("id", id)
-  
-    console.log(data);
 
     return { error, status, data };
-
-// const {
-//   data: userData,
-//   error: userError,
-//   status: userStatus,
-// } = await supabase
-//   .from("users")
-//   .select("email")
-//   .single()
-//   .eq("id", userId);
-
-// if (userData) {
-//   const { email } = await userData;
-//   return { error, status, data: { post: latestPost, userId, email } };
-// }
-
 };
--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -3,6 +3,7 @@ import styles from "./account.module.css";
 import { getUser } from "../../api-routes/user";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import useSWR from "swr";
+import { getAuthorFromEmail } from "../../utils/getAuthorFromEmail"
 
 const userCacheKey = "/account";
 
@@ -12,66 +13,26 @@ export default function Account() {
 
   const id = user?.id;
 
-  console.log(id);
-
-  const { data, error } = useSWR(id ? [userCacheKey, id] : null, () =>
-    getUser({ id })
+  const { data: { data = {} } = {}, error } = useSWR(
+    id ? `${userCacheKey}${id}` : null,
+    () => getUser(id)
   );
-
+  
   if (error) {
-    return <div>Error loading user data</div>;
+    return <div>Error loading user data: {error.message}</div>;
   }
 
   if (!data) {
     return <div>Loading user data...</div>;
   }
 
-  console.log(data);
+const userName = data.name != null ? data.name : getAuthorFromEmail(data.email)
 
   return (
     <div className={styles.container}>
       <Heading>My Account</Heading>
-      {/* Render user data here */}
+      <p>Email: {data.email}</p>
+      <p>Username: {userName}</p>
     </div>
   );
 }
-
-// import Heading from "@components/heading";
-// import styles from "./account.module.css";
-// import { getUser } from "../../api-routes/user";
-// import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
-// import useSWR from "swr";
-
-// const userCacheKey = "/account";
-
-// export default function Account() {
-//   const supabaseClient = useSupabaseClient();
-//   const user = useUser();
-
-//   const id = user ? user.id : null;
-
-//   console.log(id);
-
-//   const { data: { data = {} } = {}, error } = useSWR(
-//     id ? `${userCacheKey}${id}` : null,
-//     () => getUser({ id })
-//   );
-
-//   if (error) {
-//     return <div>Error loading user data</div>;
-//   }
-
-//   if (!data) {
-//     return <div>Loading user data...</div>;
-//   }
-
-//   console.log(data);
-
-//   return (
-//     <div className={styles.container}>
-//       <Heading>My Account</Heading>
-//       {/* <p className={styles.author}>{author}</p>
-//       <p>{comment}</p> */}
-//     </div>
-//   )
-// };
